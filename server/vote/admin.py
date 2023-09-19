2ddb89fbf342c961a18fbd6fb7193389c4f8678b
spark2357 <psbted@naver.com>
2023-09-19 23:32:03 +0900

    [feat] jwt 인증 추가

--- server/vote/admin.py
+++ server/vote/admin.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
-from .models import *
+# from django.contrib import admin
+# from .models import *
 
-admin.site.register(Poll)
-admin.site.register(Choice)
-admin.site.register(UserVote)
-admin.site.register(NonUserVote)
-admin.site.register(Comment)
-admin.site.register(Poll_Result)
+# admin.site.register(Poll)
+# admin.site.register(Choice)
+# admin.site.register(UserVote)
+# admin.site.register(NonUserVote)
+# admin.site.register(Comment)
+# admin.site.register(Poll_Result)
