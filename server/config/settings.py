f0a1d3f0d3b6f0944f4723e0a3e184182267d339
spark2357 <psbted@naver.com>
2023-10-08 23:52:46 +0900

    fix: vs_account 앱 삭제

--- server/config/settings.py
+++ server/config/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "vs_account",
     "vote",
     'accounts',
     'django.contrib.sites',
